2aec617e5f2771dd4df4659659fd87117f3aa16c
xinpingye <srtyfung@qq.com>
2023-10-27 08:30:06 +0800

    exercise OptionParser

--- cpu-intro/test1.py
+++ cpu-intro/test1.py
@@ -5,10 +5,15 @@ from optparse import OptionParser
 parser = OptionParser()
 
 parser.add_option("-f", "--file", action="store", type="string", dest="filename")
+parser.add_option("-n", type="int", dest="num")
+parser.add_option("-v", action="store_true", dest="verbose")
+parser.add_option("-q", action="store_false", dest="verbose")
 
-args = ["-f", "foo.txt"]
+args = ["-f", "foo.txt", "-n", "123", "other args","-v", "-q"]
 (options, args) = parser.parse_args(args)
 
-
+print(args)
 
 print(options.filename)
+print(options.num)
+print(options.verbose)

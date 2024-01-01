a72f71d076ffc4774b3d0ef0871059a32afaa88c
xinpingye <srtyfung@qq.com>
2024-01-01 23:53:27 +0800

    use var from sourcefile by function local var in headerfile

--- homework/current-cv/1.c
+++ homework/current-cv/1.c
@@ -2,9 +2,16 @@
 
 #include "test.h"
 
+//#include "test_two.h"
+
+int my_test_var = 100;
+
+#include "test_two.h"
+
 int main()
 {
     int id = 0;
     test0;
+    do_test_two();
     return 0;
 }
---
+++ homework/current-cv/test_two.h
@@ -0,0 +1,10 @@
+#ifndef __test_two_h__
+#define __test_two_h__
+
+void do_test_two(void)
+{
+    printf("in test_two.h print my_test_var is %d\n",my_test_var);
+    return;
+}
+
+#endif

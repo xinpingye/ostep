2e5e510cc1287558d83451fcd19d4ebdbf05ddb8
xinpingye <srtyfung@qq.com>
2024-01-10 23:34:32 +0800

    add schedule myself for SFF

--- projects/current-webserver/src/request.c
+++ projects/current-webserver/src/request.c
@@ -178,3 +178,50 @@ void request_handle(int fd) {
 	request_serve_dynamic(fd, filename, cgiargs);
     }
 }
+
+long request_handle_first(int fd, char* method, char* uri, char* version)
+{
+    char buf[MAXBUF];
+    readline_or_die(fd, buf, MAXBUF);
+    sscanf(buf, "%s %s %s", method, uri, version);
+    char filename[MAXBUF];
+    sprintf(filename, ".%s", uri);
+    struct stat sbuf;
+    stat(filename, &sbuf);
+    return sbuf.st_size;
+}
+
+void request_handle_second(int fd, char* method, char* uri, char* version)
+{
+    int is_static;
+    struct stat sbuf;
+    char filename[MAXBUF], cgiargs[MAXBUF];
+
+    printf("method:%s uri:%s version:%s\n", method, uri, version);
+    
+    if (strcasecmp(method, "GET")) {
+	request_error(fd, method, "501", "Not Implemented", "server does not implement this method");
+	return;
+    }
+    request_read_headers(fd);
+    
+    is_static = request_parse_uri(uri, filename, cgiargs);
+    if (stat(filename, &sbuf) < 0) {
+	request_error(fd, filename, "404", "Not found", "server could not find this file");
+	return;
+    }
+    
+    if (is_static) {
+	if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+	    request_error(fd, filename, "403", "Forbidden", "server could not read this file");
+	    return;
+	}
+	request_serve_static(fd, filename, sbuf.st_size);
+    } else {
+	if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+	    request_error(fd, filename, "403", "Forbidden", "server could not run this CGI program");
+	    return;
+	}
+	request_serve_dynamic(fd, filename, cgiargs);
+    }
+}
--- projects/current-webserver/src/wserver.c
+++ projects/current-webserver/src/wserver.c
@@ -2,10 +2,14 @@
 #include "request.h"
 #include "io_helper.h"
 #include <pthread.h>
+#include <limits.h>
+#include <string.h>
 
 char default_root[] = ".";
 #define MAX_BUFFER_LEN 30
 #define MAX_THREAD_POOL_SIZE 8
+#define MAXBUF (8192)
+int policy = 0;
 
 struct buffer
 {
@@ -15,6 +19,24 @@ struct buffer
 	int use_ptr;
 	int fill_ptr;
 } buf;
+
+struct item
+{
+	int fd;
+	long size;
+	char method[MAXBUF], uri[MAXBUF], version[MAXBUF];
+};
+
+struct buffer2
+{
+	struct item array[MAX_BUFFER_LEN];
+	int len;
+	int len_limit;
+	int use_ptr;
+	int fill_ptr;
+}buf2;
+
+
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
@@ -28,12 +50,33 @@ void buffer_init(int limit)
 	buf.fill_ptr = 0;
 }
 
+void buffer2_init(int limit)
+{
+	buf2.len = 0;
+	buf2.len_limit = limit;
+	buf2.use_ptr = 0;
+	buf2.fill_ptr = 0;
+
+}
+
 void put(int fd)
 {
 	assert(buf.len < buf.len_limit);
 	buf.array[buf.fill_ptr] = fd;
 	buf.len++;
 	buf.fill_ptr = (buf.fill_ptr + 1) % MAX_BUFFER_LEN;
+}
+
+void put2(int fd, long size, char method[MAXBUF], char* uri[MAXBUF], char* version[MAXBUF])
+{
+	assert(buf2.len < buf2.len_limit);
+	buf2.array[buf2.fill_ptr].fd = fd;
+	buf2.array[buf2.fill_ptr].size = size;
+	strcpy(buf2.array[buf2.fill_ptr].method, method);
+	strcpy(buf2.array[buf2.fill_ptr].uri, uri);
+	strcpy(buf2.array[buf2.fill_ptr].version, version);
+	buf2.len++;
+	buf2.fill_ptr = (buf2.fill_ptr + 1) % MAX_BUFFER_LEN;
 }
 
 int get()
@@ -45,18 +88,53 @@ int get()
 	return tmp;
 }
 
+struct item get2()
+{
+	assert(buf2.len > 0);
+	long size_less = LONG_MAX;
+	int i_less = -1;
+	for (int i = buf2.use_ptr; i != buf2.fill_ptr; i = (i + 1) % MAX_BUFFER_LEN)
+	{
+		if (size_less < buf2.array[i].size)
+		{
+			size_less = buf2.array[i].size;
+			i_less = i;
+		}
+	}
+	struct item tmp = buf2.array[i_less];
+	buf2.array[i_less] = buf2.array[buf2.use_ptr];
+	buf2.len--;
+	buf2.use_ptr = (buf2.use_ptr + 1) % MAX_BUFFER_LEN;
+	return tmp;
+}
+
 void* worker(void* arg)
 {
 	while(1)
 	{
-		pthread_mutex_lock(&mutex);
-		while(buf.len == 0)
-			pthread_cond_wait(&fill, &mutex);
-		int fd = get();
-		pthread_cond_signal(&empty);
-		pthread_mutex_unlock(&mutex);
-		request_handle(fd);
-		close_or_die(fd);
+		
+		if (policy == 0)
+		{
+			pthread_mutex_lock(&mutex);
+			while(buf.len == 0)
+				pthread_cond_wait(&fill, &mutex);
+			int fd = get();
+			pthread_cond_signal(&empty);
+			pthread_mutex_unlock(&mutex);
+			request_handle(fd);
+			close_or_die(fd);
+		}
+		else if (policy == 1)
+		{
+			pthread_mutex_lock(&mutex);
+			while(buf2.len == 0)
+				pthread_cond_wait(&fill, &mutex);
+			struct item tmp = get2();
+			pthread_cond_signal(&empty);
+			pthread_mutex_unlock(&mutex);
+			request_handle_second(tmp.fd, tmp.method, tmp.uri, tmp.version);
+			close_or_die(tmp.fd);
+		}
 	}
 }
 
@@ -77,7 +155,7 @@ int main(int argc, char *argv[]) {
 	int buffer_max_size = 10;
 	pthread_t thread_pool[MAX_THREAD_POOL_SIZE];
     
-    while ((c = getopt(argc, argv, "d:p:t:m:")) != -1)
+    while ((c = getopt(argc, argv, "d:p:t:m:s:")) != -1)
 	switch (c) {
 	case 'd':
 	    root_dir = optarg;
@@ -91,6 +169,12 @@ int main(int argc, char *argv[]) {
 	case 'p':
 	    port = atoi(optarg);
 	    break;
+	case 's':
+		if (optarg == 'f')
+			policy = 0;
+		else if (optarg == 's')
+			policy = 1;
+		break;
 	default:
 	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
 	    exit(1);
@@ -98,6 +182,7 @@ int main(int argc, char *argv[]) {
 
  
 	buffer_init(buffer_max_size);
+	buffer2_init(buffer_max_size);
 
 	threads_init(threads_nums, thread_pool);
 
@@ -111,9 +196,23 @@ int main(int argc, char *argv[]) {
 		int client_len = sizeof(client_addr);
 		int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
 		pthread_mutex_lock(&mutex);
-		while(buf.len == buf.len_limit)
-			pthread_cond_wait(&empty, &mutex);
-		put(conn_fd);
+		if (policy == 0)
+		{
+			while(buf.len == buf.len_limit)
+				pthread_cond_wait(&empty, &mutex);
+			put(conn_fd);
+		}
+		else if (policy == 1)
+		{
+			char method[MAXBUF], uri[MAXBUF], version[MAXBUF];
+			memset(method, 0, sizeof(method));
+			memset(uri, 0, sizeof(uri));
+			memset(version, 0, sizeof(version));
+			long size = request_handle_first(conn_fd, method, uri, version);
+			while(buf2.len == buf2.len_limit)
+				pthread_cond_wait(&empty, &mutex);
+			put2(conn_fd, size, method, uri, version);
+		}
 		pthread_cond_signal(&fill);
 		pthread_mutex_unlock(&mutex);
     }
